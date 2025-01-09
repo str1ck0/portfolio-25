50a6ebb1cc353b44e570a7b0d53cafc3c90e3d6a
Liam Strickland <liam.strickland96@gmail.com>
2025-01-09 17:49:12 +0200

    add link to nav logo, add albert sans font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import ThemeToggle from "@/components/ThemeToggle"
 import NavLink from "@/components/NavLink"
 import Image from "next/image";
+import Link from 'next/link'
 
 
 const Navbar = () => {
@@ -10,10 +11,12 @@ const Navbar = () => {
     <nav className="fixed top-0 w-full bg-white/80 dark:bg-gray-900/80 text-gray-900 dark:text-gray-100  backdrop-blur-sm z-50 transition-all duration-300 ease-in-out">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center flex-shrink-0">
-            <Image src="/images/logo.png" alt="Logo" width={30} height={30} unoptimized={true} quality={100} loading="eager" placeholder="empty" />
-            <span className="text-3xl ml-4 font-space font-bold">liam strickland</span>
-          </div>
+          <Link href="/" className="inline-block">
+            <div className="flex items-center flex-shrink-0">
+              <Image src="/images/logo.png" alt="Logo" width={30} height={30} unoptimized={true} quality={100} loading="eager" placeholder="empty" />
+              <span className="text-3xl ml-4 font-space font-bold">liam strickland</span>
+            </div>
+          </Link>
           <div className="hidden md:block">
             <div className="ml-10 flex items-center space-x-8 font-space">
               <NavLink href="#about">About</NavLink>
